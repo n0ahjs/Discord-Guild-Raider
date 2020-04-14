0e46f31825bc7719ebf781bd944a6baba57d7759
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-04-15 00:20:13 +0100

    ReactMessage to bypass verification channels
    ReactMessage to bypass verification channels

--- Commands/reactMessage.js
+++ Commands/reactMessage.js
@@ -2,8 +2,32 @@ const rl = require("readline-sync");
 
 module.exports.run = async(Client) => {
 	return new Promise((resolve, reject) => {
-		console.log("WIP");
-		resolve();
+		let targetGuild = rl.question("Guild ID: ");
+		let targetChannel = rl.question("Channel ID: ");
+		let reaction = rl.question("Reaction name: ");
+
+		Client.Accounts.forEach((account, index) => {
+			let instance = account.instance;
+			let Guild = instance.guilds.get(targetGuild);
+			if(Guild){
+				console.log(`Found Guild via ${instance.user.tag}!`);
+				let Channel = Guild.channels.get(targetChannel);
+				if(Channel){
+					console.log(`Found Channel via ${instance.user.tag}!`);
+					let message = Channel.fetchMessages({limit: 10}).then(messages => {
+						messages.array().forEach(async (message, index) => {
+							setTimeout(() => {
+								message.react(reaction).then(() => {
+									console.log(`Successfully reacted via ${instance.user.tag}!`);
+								}).catch(() => { console.log(`Failed to react via ${instance.user.tag}!`) });
+							}, index * 1000);
+						});
+					});
+				}
+			}
+			if(index == Client.Accounts.length - 1) resolve();
+		});
+		//resolve();
 	});
 }
 
